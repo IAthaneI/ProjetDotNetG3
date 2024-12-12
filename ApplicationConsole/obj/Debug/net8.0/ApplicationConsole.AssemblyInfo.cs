e4d79eca77ea02d59a50f91c2cd2a86504afd57f
IAthaneI <ryan.rossez@gmail.com>
2024-12-12 09:07:25 +0100

    [12/12/2024] - Correction de ClientRepository

--- ApplicationConsole/obj/Debug/net8.0/ApplicationConsole.AssemblyInfo.cs
+++ ApplicationConsole/obj/Debug/net8.0/ApplicationConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplicationConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b552fa8d715bf82d5ae0020594794e5d1c3855b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c0d13f699ec69e06639ec6ace92362fb55176a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplicationConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplicationConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
